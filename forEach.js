22e765121a7f4516e18c6b057c048575a17f1874
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2016-06-12 15:21:50 -0400

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,13 +1,13 @@
 function forEach (object, callback, scope) {
-    var result = false;
-    if (object instanceof Array || typeof object == 'string') {
+    var result = undefined;
+    if (typeof object.length != 'undefined') {
         for (var iterator = 0; iterator < object.length; iterator++) {
             result = callback.call(scope, object[iterator], iterator, object);
             if (result !== undefined) {
                 break;
             }
         }
-    } else if (object instanceof Object) {
+    } else {
         for (var property in object) {
             if (Object.prototype.hasOwnProperty.call(object, property)) {
                 result = callback.call(scope, object[property], property, object);
